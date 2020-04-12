18ec96a115cf358f92835cfd1851641aa7b72ffa
vagif4799 <quliyev_vaqif@icloud.com>
2020-04-12 08:22:44 +0400

    -- The Project initial set up is ready

---
+++ src/test/java/com/vagif/mssc/MsscBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagif.mssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

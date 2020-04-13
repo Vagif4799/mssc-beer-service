6eb7aa02a9f63fb84a5b85a6b1c5743661398db5
vagif4799 <quliyev_vaqif@icloud.com>
2020-04-13 13:57:54 +0400

    -- Beer.java and BeerRepository.java added

---
+++ src/main/java/com/vagif/mssc/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.vagif.mssc.repositories;
+
+import com.vagif.mssc.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
